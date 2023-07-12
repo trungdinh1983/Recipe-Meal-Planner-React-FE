03706fc9f166f508a3e31ea8d7f772f17a9d1511
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-07-12 13:59:30 -0700

    Index w Errors

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { IngredientsIndex } from "./IngredientsIndex";
+
+const [ingredients, setIngredients] = useState([]);
+
+const handleIndexIngredients = () => {
+  console.log("handleIndexIngredients");
+  axios.get("http://localhost:3000/ingredients.json").then((response) => {
+    console.log(response.data);
+    setIngredients(response.data);
+  });
+};
+
+useEffect(handleIndexIngredients, []);
 
 export function Content() {
   return (
--- src/IngredientsIndex.jsx
+++ src/IngredientsIndex.jsx
@@ -1,7 +1,14 @@
-export function IngredientsIndex() {
+export function IngredientsIndex(props) {
   return (
     <div>
-      <h1>All Ingredients</h1>
+              <h1>All ingredients</h1>
++       {props.ingredients.map((ingredient) => (
++         <div key={ingredient.id}>
++           <h2>{ingredient.name}</h2>
++           <img src={ingredient_image.url} />
++         
++         </div>
++       ))}
     </div>
   );
 }
