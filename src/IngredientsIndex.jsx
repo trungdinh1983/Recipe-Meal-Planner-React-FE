9910b6ab1035f580ea286cd1c33e7aaac43df433
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-07-13 09:18:04 -0700

    Fix Index

--- src/IngredientsIndex.jsx
+++ src/IngredientsIndex.jsx
@@ -1,14 +1,13 @@
 export function IngredientsIndex(props) {
   return (
     <div>
-              <h1>All ingredients</h1>
-+       {props.ingredients.map((ingredient) => (
-+         <div key={ingredient.id}>
-+           <h2>{ingredient.name}</h2>
-+           <img src={ingredient_image.url} />
-+         
-+         </div>
-+       ))}
+      <h1>All ingredients</h1>
+      {props.ingredients?.map((ingredient) => (
+        <div key={ingredient.id}>
+          <h2>{ingredient.name}</h2>
+          <img src={ingredient.image_url} />
+        </div>
+      ))}
     </div>
   );
 }
