94b9f54b45dba47ade7b78920f5153f5f8aaf4c9
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-07-19 09:49:54 -0700

    fix links

--- src/PantryItemNew.jsx
+++ src/PantryItemNew.jsx
@@ -19,6 +19,33 @@ export function PantryItemNew(props) {
         </div>
         <button type="submit">Add Item to Pantry</button>
       </form>
+      <br />
+      {/* <a href="http://localhost:5173/#">Home</a>
+      <br /> */}
+
+      <a href="http://localhost:5173/">Home</a>
+      <br />
+
+      <br />
+
+      <div class="dropdown">
+        <button class="dropbtn">
+          <img src="https://images.cooltext.com/5664746.gif"></img>
+        </button>
+      </div>
+      <br />
+
+      <a href="src/spaghetti.html">Spaghetti Meatball</a>
+      <br />
+      <a href="src/burger.html">Hamburger</a>
+      <br />
+      <a href="src/hotdog.html">Hot Dog</a>
+      <br />
+      <a href="src/shortrib.html">Shortrib</a>
+      <br />
+      <a href="src/grillsteak.html">Grill Steak</a>
+      <br />
+      <a href="src/mangosalsa.html">Mango Salsa</a>
     </div>
   );
 }
